6b84d4235c3f2c71d1445a7c7cf4e5f267c4c90d
Opilane TTHK <189960872+Siim-byte@users.noreply.github.com>
2026-03-20 11:05:44 +0200

    Fix error

--- Nexus.Core/Domain/Product.cs
+++ Nexus.Core/Domain/Product.cs
@@ -2,11 +2,11 @@
 {
     public class Product
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Quality { get; set; }
-        public int Quantity { get; set; }
+        public int Stock { get; set; }
         public string Description { get; set; }
 
     }
