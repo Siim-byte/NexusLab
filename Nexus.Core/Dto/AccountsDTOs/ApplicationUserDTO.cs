4bdb3fea2b3a11c21bd15315d01ccca42fdafba9
SavvaS250 <smirnyaginsavva@gmail.com>
2026-03-20 10:48:12 +0200

    Account DTO and services

---
+++ Nexus.Core/Dto/AccountsDTOs/ApplicationUserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nexus.Core.Dto.AccountsDTOs
+{
+    public class ApplicationUserDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool ProfileType { get; set; }
+        public List<Guid>? CommentIDs { get; set; }
+        public string DisplayName { get; set; }
+        public string? Gender { get; set; }
+        public int? Age { get; set; }
+    }
+}
