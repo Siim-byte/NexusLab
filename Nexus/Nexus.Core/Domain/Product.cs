e8d0eff3f30a94a7d2a4be38e00a3fa32d112f87
Opilane TTHK <189960872+Siim-byte@users.noreply.github.com>
2026-03-20 10:28:43 +0200

    Base structure

---
+++ Nexus/Nexus.Core/Domain/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Nexus.Nexus.Core.Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Quality { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+
+    }
+}
