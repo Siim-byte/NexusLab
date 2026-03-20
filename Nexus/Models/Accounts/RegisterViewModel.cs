a85d6957cf8971518aa58ccd2458e07d94e7340d
SavvaS250 <smirnyaginsavva@gmail.com>
2026-03-20 10:29:48 +0200

    Register and account things added

---
+++ Nexus/Models/Accounts/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nexus.Models.Accounts
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [Display(Name = "Sisesta parool uuesti:")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Paroolid ei kattu, kontrolli, et oled samamoodi sisestanud.")]
+        public string ConfirmPassword { get; set; }
+        public string? DisplayName { get; set; }
+        public bool ProfileType { get; set; } = false;
+
+    }
+}
