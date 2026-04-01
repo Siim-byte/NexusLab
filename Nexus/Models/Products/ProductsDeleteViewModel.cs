ed74f666ea9fd1efdd1b97cfe8a8e1b169f9f0fa
Siim-byte <beloimiis@gmail.com>
2026-04-01 20:01:55 +0300

    Product delete finished

---
+++ Nexus/Models/Products/ProductsDeleteViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Nexus.Models.Products
+{
+    public class ProductsDeleteViewModel
+    {
+        public Guid? ProductId { get; set; }
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public string? Quality { get; set; }
+        public int? Stock { get; set; }
+        public string? Description { get; set; }
+    }
+}
