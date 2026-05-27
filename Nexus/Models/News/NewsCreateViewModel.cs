08a15e952ffa4248afe10deeba5c51bc07c1dee7
SavvaS250 <smirnyaginsavva@gmail.com>
2026-05-27 10:42:34 +0300

    Create, index view models

---
+++ Nexus/Models/News/NewsCreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nexus.Models.News
+{
+    public class NewsCreateViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
