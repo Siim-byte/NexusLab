060071b4d5bbabdff9fe5a0d3bac1388d9ba862f
SavvaS250 <smirnyaginsavva@gmail.com>
2026-05-27 11:01:54 +0300

    Now you can see news

--- Nexus/Controllers/HomeController.cs
+++ Nexus/Controllers/HomeController.cs
@@ -1,21 +1,38 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nexus.Data;
 using Nexus.Models;
+using Nexus.Models.News;
+using System.Diagnostics;
 
 namespace Nexus.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var news = _context.News
+            .OrderByDescending(x => x.CreatedAt)
+            .Take(5)
+            .Select(x => new NewsIndexViewModel
+            {
+                NewsId = x.NewsId,
+                Title = x.Title,
+                Content = x.Content,
+                CreatedAt = x.CreatedAt
+            })
+            .ToList();
+
+            return View(news);
         }
 
         public IActionResult Privacy()
