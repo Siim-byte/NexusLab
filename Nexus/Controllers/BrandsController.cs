5367357edb0f78b907f7fc8d3df0c10a3c603920
Siim-byte <beloimiis@gmail.com>
2026-05-28 19:35:48 +0300

    Delete

--- Nexus/Controllers/BrandsController.cs
+++ Nexus/Controllers/BrandsController.cs
@@ -5,6 +5,7 @@ using Nexus.Core.Dto;
 using Nexus.Core.SeviceInterfrace;
 using Nexus.Data;
 using Nexus.Models.Brands;
+using Nexus.Models.Products;
 
 namespace Nexus.Controllers
 {
@@ -56,6 +57,53 @@ namespace Nexus.Controllers
             ModelState.AddModelError("", "VIGA");
             //}
             return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var brand = await _brandsServices.DetailsAsync(id);
+
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+
+            var vm = new BrandsDeleteViewModel();
+            vm.Id = brand.Id;
+            vm.Name = brand.Name;
+            vm.Slogan = brand.Slogan;
+
+            return View(vm);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(Guid Id)
+        {
+            var brand = await _brandsServices.Delete(Id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var brand = await _brandsServices.DetailsAsync(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            var vm = new BrandsDetailsViewModel()
+            {
+                Id = brand.Id,
+                Name = brand.Name,
+                Slogan = brand.Slogan
+            };
+
+            return View(vm);
         }
     }
 }
---
+++ Nexus/Models/Brands/BrandsDetailsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Nexus.Models.Brands
+{
+    public class BrandsDetailsViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Slogan { get; set; }
+    }
+}
